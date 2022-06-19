19f82ad04c6522a454bde7e687dc788941f8941e
«Egor <egoled8@gmail.com>
2022-06-19 20:41:51 +0300

    вторая задача от 16.06

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+string Num = Console.ReadLine(); 
+int N = int.Parse(Num);
+int Sum = 0;
+
+while (N>0)
+{
+    Sum = Sum + N % 10;
+    N = (N-N % 10)/10;
+}
+
+System.Console.WriteLine($"Сумма цифр в числе равна {Sum}");
