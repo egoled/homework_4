766d0a833324a32dbb56ee7bd3fe467bd1bcd0ce
«Egor <egoled8@gmail.com>
2022-06-19 20:00:50 +0300

    первая задача от 16.06 с правкой

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -1,4 +1,6 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B 
+// Показателем нат. степени является натуральное число.
+//  натуральными называют целые числа, которые больше нуля
 Console.WriteLine("Введите число A: ");
 string NumberA = Console.ReadLine(); 
 int A = int.Parse(NumberA);
@@ -7,9 +9,16 @@ string NumberB = Console.ReadLine();
 int B = int.Parse(NumberB);
 int result = 1;
 
+if (B>0)
+{
 for (int i=0; i<B; i++)
 {
     result = result*A;
 }
-
 Console.WriteLine($"A в натуральной степени B это {result}");
+}
+else
+{
+    System.Console.WriteLine("B не натуральное число");
+}
+
